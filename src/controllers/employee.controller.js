77f087f527824abfd612cff9442ff14e438bce1f
javafullstackD <javafullstackdeveloper705@gmail.com>
2022-05-30 10:38:16 +0200

    all employees data fetched

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -4,5 +4,13 @@ const EmployeeModel = require('../models/employee.model');
 //get all employee list
 
 exports.getEmployeeList = (req,res)=>{
-   console.log('Here are all employee List');
+   // console.log('Here are all employee List');
+   EmployeeModel.getAllEmployees((err,employees)  =>{
+      console.log('We are here');
+      
+      if(err)
+      res.send(err);
+      console.log('Employees',employees);
+      res.send(employees)
+   })
 }
